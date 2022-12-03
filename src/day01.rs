6739b014f096fed10cb49e81c23ae2a6e7c655ec
Belén Albeza <belen@hey.com>
2022-12-03 14:57:32 +0100

    implement day 1 (part 2)

--- src/day01.rs
+++ src/day01.rs
@@ -32,11 +32,27 @@ pub fn parse_input(input: &str) -> Vec<Elf> {
 
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &[Elf]) -> u32 {
-    return input
+    input
         .into_iter()
         .map(|x| x.total_calories())
         .max()
-        .unwrap_or(0);
+        .unwrap_or(0)
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &[Elf]) -> u32 {
+    let mut calories_by_elf: Vec<u32> = input.into_iter().map(|x| x.total_calories()).collect();
+
+    const N: u32 = 3;
+    let mut top_calories: Vec<u32> = vec![];
+
+    for _ in 0..N {
+        let max = calories_by_elf.clone().into_iter().max().unwrap_or(0);
+        top_calories.push(max);
+        calories_by_elf.remove(calories_by_elf.iter().position(|&x| x == max).unwrap());
+    }
+
+    top_calories.into_iter().sum()
 }
 
 #[cfg(test)]
@@ -53,5 +69,17 @@ mod tests {
             vec![10000],
         ];
         assert_eq!(solve_part1(&input), 24000);
+    }
+
+    #[test]
+    fn test_day1_part2() {
+        let input: Vec<Elf> = vec![
+            vec![1000, 2000, 3000],
+            vec![4000],
+            vec![5000, 6000],
+            vec![7000, 8000, 9000],
+            vec![10000],
+        ];
+        assert_eq!(solve_part2(&input), 45000);
     }
 }
