c29e6048f27e787504d5709da3bb3199d515b628
Belén Albeza <belen@hey.com>
2022-12-05 14:57:43 +0100

    implement day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -11,7 +11,7 @@ pub type Cargo = Vec<Stack>;
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Move {
-    amount: u64,
+    amount: usize,
     from: usize,
     to: usize,
 }
@@ -34,11 +34,34 @@ impl Ship {
         self.cargo[to].push(cargo_crate);
     }
 
-    fn operate_crane(&mut self, moves: &[Move]) {
+    fn move_crate_stack(&mut self, from: usize, to: usize, size: usize) {
+        let new_len = self.cargo[from].len() - size;
+
+        // NOTE: this is go get away with Rust's borrow checker, since we need to do multiple
+        // .     mutable borrows and it doesn't like it…
+        let stacks = self.cargo.split_at_mut(std::cmp::max(from, to));
+        let (from_stack, to_stack) = if from > to {
+            (&mut stacks.1[0], &mut stacks.0[to])
+        } else {
+            (&mut stacks.0[from], &mut stacks.1[0])
+        };
+
+        for cargo_crate in from_stack.drain(new_len..) {
+            to_stack.push(cargo_crate)
+        }
+    }
+
+    fn operate_crane_v1(&mut self, moves: &[Move]) {
         for crane_move in moves {
             for _ in 0..crane_move.amount {
                 self.move_crate(crane_move.from, crane_move.to);
             }
+        }
+    }
+
+    fn operate_crane_v2(&mut self, moves: &[Move]) {
+        for crane_move in moves {
+            self.move_crate_stack(crane_move.from, crane_move.to, crane_move.amount)
         }
     }
 }
@@ -79,14 +102,14 @@ fn parse_moves(input: &str) -> Vec<Move> {
         .lines()
         .into_iter()
         .map(|line| {
-            let numbers: Vec<u64> = RE
+            let numbers: Vec<usize> = RE
                 .find_iter(line)
-                .map(|x| x.as_str().parse::<u64>().unwrap())
+                .map(|x| x.as_str().parse::<usize>().unwrap())
                 .collect();
             Move {
                 amount: numbers[0],
-                from: (numbers[1] - 1) as usize,
-                to: (numbers[2] - 1) as usize,
+                from: (numbers[1] - 1),
+                to: (numbers[2] - 1),
             }
         })
         .collect()
@@ -97,7 +120,16 @@ pub fn solve_part1(input: &(Cargo, Vec<Move>)) -> String {
     let mut ship = Ship {
         cargo: input.0.clone(),
     };
-    ship.operate_crane(&input.1);
+    ship.operate_crane_v1(&input.1);
+    ship.top_cargo()
+}
+
+#[aoc(day5, part2)]
+pub fn solve_part2(input: &(Cargo, Vec<Move>)) -> String {
+    let mut ship = Ship {
+        cargo: input.0.clone(),
+    };
+    ship.operate_crane_v2(&input.1);
     ship.top_cargo()
 }
 
@@ -152,6 +184,36 @@ mod tests {
             ],
         );
         assert_eq!(solve_part1(&input), "CMZ");
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        let input = (
+            vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']],
+            vec![
+                Move {
+                    amount: 1,
+                    from: 1,
+                    to: 0,
+                },
+                Move {
+                    amount: 3,
+                    from: 0,
+                    to: 2,
+                },
+                Move {
+                    amount: 2,
+                    from: 1,
+                    to: 0,
+                },
+                Move {
+                    amount: 1,
+                    from: 0,
+                    to: 1,
+                },
+            ],
+        );
+        assert_eq!(solve_part2(&input), "MCD");
     }
 
     #[test]
